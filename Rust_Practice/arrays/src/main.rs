a9d0476ce05185c38e145204a247d54199ee6ff7
tehilafavourite <tehilafavourite@gmail.com>
2025-05-14 12:50:03 +0100

    initializing array with different methods

---
+++ Rust_Practice/arrays/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // initialize an array with different methods
+    let arr1 = [1, 2, 3, 4, 5];
+    let arr2: [i32; 5] = [1, 2, 3, 4, 5];
+    let arr3 = [1; 5]; // all elements are initialized to 1
+    println!("{:?}, {:?}, {:?}", arr1, arr2, arr3);
+    
+}
