9b5f9dc22b7e5e65d2838f570a4c3dc38c5f2095
tehilafavourite <tehilafavourite@gmail.com>
2025-05-14 12:51:00 +0100

    accessing the elements of an array

--- Rust_Practice/arrays/src/main.rs
+++ Rust_Practice/arrays/src/main.rs
@@ -4,5 +4,10 @@ fn main() {
     let arr2: [i32; 5] = [1, 2, 3, 4, 5];
     let arr3 = [1; 5]; // all elements are initialized to 1
     println!("{:?}, {:?}, {:?}", arr1, arr2, arr3);
+    // accessing elements of an array
+    let first = arr1[0];
+    let second = arr1[1];
+    let third = arr1[2];
+    println!("first: {}, second: {}, third: {}", first, second, third);
     
 }
