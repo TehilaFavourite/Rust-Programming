72ade82d476ad81aacc661ba3f8f6192e4a82067
tehilafavourite <tehilafavourite@gmail.com>
2025-05-14 12:40:11 +0100

    creating and destructuring tuples

---
+++ Rust_Practice/tuples/target/debug/deps/tuples-d147d76a69f02942.d
@@ -0,0 +1,5 @@
+/Users/favourajaye/Desktop/workspace/rust/Rust_Practice/tuples/target/debug/deps/tuples-d147d76a69f02942: src/main.rs
+
+/Users/favourajaye/Desktop/workspace/rust/Rust_Practice/tuples/target/debug/deps/tuples-d147d76a69f02942.d: src/main.rs
+
+src/main.rs:
