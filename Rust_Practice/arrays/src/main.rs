fb0cc64380db3a9e4ff87308f33f650e113f3e4f
tehilafavourite <tehilafavourite@gmail.com>
2025-05-14 12:51:56 +0100

    iterating over an array

--- Rust_Practice/arrays/src/main.rs
+++ Rust_Practice/arrays/src/main.rs
@@ -9,5 +9,24 @@ fn main() {
     let second = arr1[1];
     let third = arr1[2];
     println!("first: {}, second: {}, third: {}", first, second, third);
-    
+    // access array elements safely with bounds checking
+    if let Some(fourth) = arr1.get(3) {
+        println!("fourth: {}", fourth);
+    } else {
+        println!("Index out of bounds");
+    }
+    // iterating over an array
+    for i in 0..arr1.len() {
+        println!("arr1[{}]: {}", i, arr1[i]);
+    }
+    // iterating over an array with for loop
+    for &item in arr1.iter() {
+        println!("item: {}", item);
+    }
+    // iterating over an array with while loop
+    let mut i = 0;
+    while i < arr1.len() {
+        println!("arr1[{}]: {}", i, arr1[i]);
+        i += 1;
+    }
 }
