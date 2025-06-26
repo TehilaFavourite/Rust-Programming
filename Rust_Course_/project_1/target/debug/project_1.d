1a7da2673114caec086db7567fdcd279008931fc
tehilafavourite <tehilafavourite@gmail.com>
2025-06-27 00:07:19 +0100

    project1

---
+++ Rust_Course_/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\NetzenceBD\OneDrive\Desktop\P\ workspace\Rust-Programming\Rust_Course_\project_1\target\debug\project_1.exe: C:\Users\NetzenceBD\OneDrive\Desktop\P\ workspace\Rust-Programming\Rust_Course_\project_1\src\main.rs
