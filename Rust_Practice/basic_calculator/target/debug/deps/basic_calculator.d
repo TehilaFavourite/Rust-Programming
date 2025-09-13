4571f249a365aef59789b2181f45559f4f4111a6
tehilafavourite <tehilafavourite@gmail.com>
2025-09-13 13:05:11 +0100

    Rust basic calculator

---
+++ Rust_Practice/basic_calculator/target/debug/deps/basic_calculator.d
@@ -0,0 +1,5 @@
+C:\Users\NetzenceBD\OneDrive\Desktop\P workspace\Rust-Programming\Rust_Practice\basic_calculator\target\debug\deps\basic_calculator.exe: src\main.rs
+
+C:\Users\NetzenceBD\OneDrive\Desktop\P workspace\Rust-Programming\Rust_Practice\basic_calculator\target\debug\deps\basic_calculator.d: src\main.rs
+
+src\main.rs:
