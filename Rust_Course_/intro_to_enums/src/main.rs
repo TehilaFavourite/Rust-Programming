b6d268829f1cabb36bfd4e9a6a7f69b5518007a8
tehilafavourite <tehilafavourite@gmail.com>
2025-10-30 11:55:03 +0100

    intro to enums

--- Rust_Course_/intro_to_enums/src/main.rs
+++ Rust_Course_/intro_to_enums/src/main.rs
@@ -7,8 +7,16 @@ enum CardSuits {
     Clubs,
 }
 
+struct Card {
+    rank: String,
+    suit: CardSuits,
+}
+
 fn main() {
     let first_card = CardSuits::Hearts;
     let mut second_card = CardSuits::Diamonds;
     println!("This is the second card: {:?}", second_card);
+    
+    let card_suits = [CardSuits::Hearts, CardSuits::Clubs];
+    println!("This is the card_suits: {:?}", card_suits);
 }
