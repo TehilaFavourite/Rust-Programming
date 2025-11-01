550097ca9657200dd016306f52ca43c7fa85a7a9
tehilafavourite <tehilafavourite@gmail.com>
2025-11-01 01:03:03 +0100

    generics in structs

---
+++ Rust_Course_/generics_in_structs/target/debug/deps/generics_in_structs.d
@@ -0,0 +1,5 @@
+C:\Users\NetzenceBD\Desktop\Desktop\P workspace\Rust-Programming\Rust_Course_\generics_in_structs\target\debug\deps\generics_in_structs.exe: src\main.rs
+
+C:\Users\NetzenceBD\Desktop\Desktop\P workspace\Rust-Programming\Rust_Course_\generics_in_structs\target\debug\deps\generics_in_structs.d: src\main.rs
+
+src\main.rs:
