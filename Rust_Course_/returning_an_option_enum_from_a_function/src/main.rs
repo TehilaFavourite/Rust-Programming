760a5b30d241d78d6bb7a00df2051909d5d1e580
tehilafavourite <tehilafavourite@gmail.com>
2025-11-18 05:07:19 +0100

    returning an option enum from a function

---
+++ Rust_Course_/returning_an_option_enum_from_a_function/src/main.rs
@@ -0,0 +1,14 @@
+fn is_item_in_stock(item_is_in_system: bool, item_is_in_stock: bool) -> Option<bool> {
+    if item_is_in_system && item_is_in_stock {
+        Option::Some(true)
+    } else if item_is_in_system {
+        Option::Some(false)
+    } else {
+        Option::None
+    }
+}
+
+fn main() {
+    let availability = is_item_in_stock(true, true);
+    println!("{availability:?}");
+}
