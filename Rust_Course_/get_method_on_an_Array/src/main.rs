e4b214d347a8b4e16eac846c1eb9278e0367d070
tehilafavourite <tehilafavourite@gmail.com>
2025-11-18 04:28:09 +0100

    get method on an array

--- Rust_Course_/get_method_on_an_Array/src/main.rs
+++ Rust_Course_/get_method_on_an_Array/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
-    let musical_instrument = [String::from("Guitar"), String::from("Drums"), String::from("bass"),];
-    let first_instrument = musical_instrument.get(0);
-    match first_instrument {
-        Some(instrument) => println!("The first instrument is: {}", instrument),
-        None => println!("There is no instrument at index 0"),
-    }
+    let musical_instrument = [
+        String::from("Guitar"),
+        String::from("Drums"),
+        String::from("bass"),
+    ];
+    let bass: Option<&String> = musical_instrument.get(2);
+    println!("{:?}", bass);
+
+    let invalid = musical_instrument.get(100);
+    println!("{:?}", invalid);
 }
---
+++ Rust_Course_/get_method_on_an_Array/target/debug/deps/get_method_on_an_Array.d
@@ -0,0 +1,5 @@
+C:\Users\NetzenceBD\Desktop\Desktop\P workspace\Rust-Programming\Rust_Course_\get_method_on_an_Array\target\debug\deps\get_method_on_an_Array.exe: src\main.rs
+
+C:\Users\NetzenceBD\Desktop\Desktop\P workspace\Rust-Programming\Rust_Course_\get_method_on_an_Array\target\debug\deps\get_method_on_an_Array.d: src\main.rs
+
+src\main.rs:
