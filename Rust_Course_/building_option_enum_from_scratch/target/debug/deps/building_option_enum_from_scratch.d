91c70bce3f9f25605893ca9a9fe09a10bc9d1fc7
tehilafavourite <tehilafavourite@gmail.com>
2025-12-02 16:08:09 +0100

    the unwrap_or

---
+++ Rust_Course_/building_option_enum_from_scratch/target/debug/deps/building_option_enum_from_scratch.d
@@ -0,0 +1,5 @@
+C:\Users\NetzenceBD\Desktop\Desktop\P workspace\Rust-Programming\Rust_Course_\building_option_enum_from_scratch\target\debug\deps\building_option_enum_from_scratch.exe: src\main.rs
+
+C:\Users\NetzenceBD\Desktop\Desktop\P workspace\Rust-Programming\Rust_Course_\building_option_enum_from_scratch\target\debug\deps\building_option_enum_from_scratch.d: src\main.rs
+
+src\main.rs:
