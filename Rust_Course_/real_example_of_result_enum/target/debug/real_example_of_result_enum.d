d54d100ca27f523e1c2169a9e52eef58f987e27b
tehilafavourite <tehilafavourite@gmail.com>
2025-12-09 05:59:00 +0100

    real example of result enum

---
+++ Rust_Course_/real_example_of_result_enum/target/debug/real_example_of_result_enum.d
@@ -0,0 +1,1 @@
+C:\Users\NetzenceBD\Desktop\Desktop\P\ workspace\Rust-Programming\Rust_Course_\real_example_of_result_enum\target\debug\real_example_of_result_enum.exe: C:\Users\NetzenceBD\Desktop\Desktop\P\ workspace\Rust-Programming\Rust_Course_\real_example_of_result_enum\src\main.rs
