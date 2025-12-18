e21f90de79cfdce055af754195849b759f2a9bc2
tehilafavourite <tehilafavourite@gmail.com>
2025-12-18 21:58:19 +0100

    removing an element

--- Rust_Course_/Vector_Folder/add_remove_elements/src/main.rs
+++ Rust_Course_/Vector_Folder/add_remove_elements/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
 
     pizza_diameters.insert(3, 13);
     println!("{pizza_diameters:?}");
+
+    let pizza_diameters = pizza_diameters.pop();
+    println!("{pizza_diameters:?}");
+
+    let mut pizza = vec![8, 10, 12, 14];
+    let third = pizza.remove(2);
+    println!("{pizza:?}");
 }
