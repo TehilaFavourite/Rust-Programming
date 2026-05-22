c08aae38cdeed8f11a6baa9463e28a696e17c714
TehilaFavourite <tehilafavourite@gmail.com>
2026-05-22 17:45:30 +0100

    lifetime elision rules

---
+++ Rust_Course_/target/debug/deps/lifetime_elision_rules-84516282cda551f9.d
@@ -0,0 +1,9 @@
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/lifetime_elision_rules-84516282cda551f9.d: lifetimes/lifetime_elision_rules/src/main.rs Cargo.toml
+
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/liblifetime_elision_rules-84516282cda551f9.rmeta: lifetimes/lifetime_elision_rules/src/main.rs Cargo.toml
+
+lifetimes/lifetime_elision_rules/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ Rust_Course_/target/debug/deps/lifetime_elision_rules-d6a5602890f54c31.d
@@ -0,0 +1,9 @@
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/lifetime_elision_rules-d6a5602890f54c31.d: lifetimes/lifetime_elision_rules/src/main.rs Cargo.toml
+
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/liblifetime_elision_rules-d6a5602890f54c31.rmeta: lifetimes/lifetime_elision_rules/src/main.rs Cargo.toml
+
+lifetimes/lifetime_elision_rules/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
