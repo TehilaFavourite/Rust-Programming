ebeb6839f38011c10ccd6938890618d5c2fc59e6
TehilaFavourite <tehilafavourite@gmail.com>
2026-07-09 16:07:51 +0100

    the for loop with iterator

---
+++ Rust_Course_/target/debug/deps/the_for_loop_with_iterator-8b3d47c6bc0d8058.d
@@ -0,0 +1,9 @@
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/the_for_loop_with_iterator-8b3d47c6bc0d8058.d: iterations/the_for_loop_with_iterator/src/main.rs Cargo.toml
+
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/libthe_for_loop_with_iterator-8b3d47c6bc0d8058.rmeta: iterations/the_for_loop_with_iterator/src/main.rs Cargo.toml
+
+iterations/the_for_loop_with_iterator/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ Rust_Course_/target/debug/deps/the_for_loop_with_iterator-b819fdde810f0bff.d
@@ -0,0 +1,9 @@
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/the_for_loop_with_iterator-b819fdde810f0bff.d: iterations/the_for_loop_with_iterator/src/main.rs Cargo.toml
+
+/Users/teheelaa/tehila_workspace/rust/Rust-Programming/Rust_Course_/target/debug/deps/libthe_for_loop_with_iterator-b819fdde810f0bff.rmeta: iterations/the_for_loop_with_iterator/src/main.rs Cargo.toml
+
+iterations/the_for_loop_with_iterator/src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
